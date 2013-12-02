062ea4fe9b254b735c5b5396254b901c8a4d3aea
mihir <mihir.khatwani@gmail.com>
2013-12-02 13:52:55 +0530

    Forgot to remove override from the implementation

--- src/Length.java
+++ src/Length.java
@@ -7,10 +7,6 @@ public class Length extends AMeasure {
         super(value, unitName);
     }
 
-    @Override
-    protected double getConversionFactor(Unit unit1, Unit unit2) {
-        return validUnits.get(unit1)/validUnits.get(unit2);
-    }
 
     @Override
     protected HashMap<Unit, Double> initializeValidUnits(HashMap<Unit, Double> validUnits) {
--- src/Weight.java
+++ src/Weight.java
@@ -8,11 +8,6 @@ public class Weight extends AMeasure {
     }
 
     @Override
-    protected double getConversionFactor(Unit unit1, Unit unit2) {
-        return validUnits.get(unit1)/validUnits.get(unit2);
-    }
-
-    @Override
     protected HashMap<Unit, Double> initializeValidUnits(HashMap<Unit, Double> validUnits) {
         validUnits.put(Unit.GRAM, 1d);
         validUnits.put(Unit.KILOGRAM, 1000d);
